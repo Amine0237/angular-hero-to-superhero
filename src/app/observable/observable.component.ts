ce82cf3bac4cf0acc0bc5cdb0f34ab5f91aa98a3
Amine0237 <chadli.medamin@gmail.com>
2024-07-17 13:16:18 +0100

    just finnished the third vid

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./observable.component.scss']
 })
 export class ObservableComponent {
+  data: number[] = [];
+  count = 0;
+  myObservable: any;
+
+  constructor(){}
+
+  ngOnInit():void {
+    this.myObservable = new Observable<number[]>((observer) => {
+      setInterval(() => {
+        this.count++;
+        this.data.push(this.count);
+        
+        if(this.count < 5) {
+          observer.next(this.data);
+        } else {
+          observer.complete();
+        }
+      }, 1000);
+    })
+  }
+
+  getData(){
+    this.myObservable.subscribe((res: any) => {
+      console.log(res);
+    }, (err: any) => {
+      console.log(err);
+    }, () => {
+      console.log('Completed');
+    });
+    console.log(this.data);
+  }
 
 }
--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./promise.component.scss']
 })
 export class PromiseComponent {
+  isOnline = false;
+  status = 'offline';
+  myPromise: any;
+
+  constructor(){}
+
+  ngOnInit():void {
+    this.getStatus();
+    this.myPromise.then((res: any) => {
+      console.log(res);
+      this.checkLogic();
+    }, 
+    (err: any) => {
+      console.log(err);
+    });
+    
+  }
+
+  getStatus(){
+    this.myPromise = new Promise<boolean>((resolve, reject) => {
+      setTimeout(() => {
+        this.isOnline = true;
+        resolve(this.isOnline);
+      }, 100);
+    })
+    
+  }
+
+  checkLogic(){
+    this.status = this.isOnline?'online':'offline';
+  }
 
 }
